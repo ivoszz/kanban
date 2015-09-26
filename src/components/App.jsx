83334bdac199bf893dd801b15767ef3bbacd807e
ivoszz <ivoszz@gmail.com>
2015-09-26 13:04:43 +0200

    Basic Note app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,82 @@
 import React, {Component} from 'react';
-import Note from './Note.jsx';
+import Notes from './Notes.jsx';
+import uuid from 'node-uuid';
 
 export default class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        }, {
+          id: uuid.v4(),
+          task: 'Learn React'
+        }, {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+
+    this.findNote = this.findNote.bind(this);
+    this.addNote = this.addNote.bind(this);
+    this.editNote = this.editNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+  }
+
+  findNote (id) {
+    const notes = this.state.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if (noteIndex < 0) {
+      console.warn(`Failed to find note ${notes}, ${id}`);
+    }
+
+    return noteIndex;
+  }
+
+  addNote () {
+    this.setState({
+      notes: this.state.notes.concat([{id: uuid.v4(), task: 'New task'}])
+    });
+  }
+
+  editNote (id, task) {
+    let notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if (noteIndex < 0) {
+      return;
+    }
+
+    notes[noteIndex].task = task;
+    this.setState({notes});
+  }
+
+  deleteNote (id) {
+    let notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if (noteIndex < 0) {
+      return;
+    }
+
+    this.setState({
+      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+    });
+  }
+
   render () {
+    const notes = this.state.notes;
     return (
-      <Note />
+      <div>
+        <button className='add-note' onClick={this.addNote}>+</button>
+        <Notes items={notes}
+          onEdit={this.editNote} onDelete={this.deleteNote} />
+      </div>
     );
   }
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,73 @@
 import React, {Component} from 'react';
 
-export default class App extends Component {
+export default class Note extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+
+    this.edit = this.edit.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.renderTask = this.renderTask.bind(this);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.renderDelete = this.renderDelete.bind(this);
+  }
+
+  edit () {
+    this.setState({
+      editing: true
+    });
+  }
+
+  finishEdit (e) {
+    this.props.onEdit(e.target.value);
+
+    this.setState({
+      editing: false
+    });
+  }
+
+  checkEnter (e) {
+    if (e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
   render () {
+    const editing = this.state.editing;
+
     return (
-      <div>Learn Webpack</div>
+      <div>
+        {editing ? this.renderEdit() : this.renderTask()}
+      </div>
+    );
+  }
+
+  renderTask () {
+    const onDelete = this.props.onDelete;
+
+    return (
+      <div onClick={this.edit}>
+        <span className='task'>{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null}
+      </div>
+    );
+  }
+
+  renderEdit () {
+    return (
+      <input type='text' autoFocus={true} defaultValue={this.props.task}
+        onBlur={this.finishEdit} onKeyPress={this.checkEnter} />
+    );
+  }
+
+  renderDelete () {
+    return (
+      <button className='delete' onClick={this.props.onDelete}>x</button>
     );
   }
 }
